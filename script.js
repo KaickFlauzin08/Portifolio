a8e396ff6af95b228bb9e48aaceb3d3ce087e3c7
KaickFlauzin08 <Kaickflauzino7@gmail.com>
2025-09-25 09:42:07 -0300

    atualizado-1

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// script.js
-
 function irParaPaginaSobre() {
     window.location.href = 'contato.html';
 }
@@ -7,3 +5,33 @@ function irParaPaginaSobre() {
 function VoltaparaoComeço() {
     window.location.href = 'index.html';
 }
+
+
+
+
+
+// === AVISOOOOOOOOOOO DO CONTATOOOOOOOOOOOOOO === //
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('form');
+
+
+    form.addEventListener('submit', function(event) {
+        
+
+        event.preventDefault();
+
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const message = document.getElementById('message').value;
+
+
+        const confirmationMessage = `
+            Obrigado por enviar sua mensagem, ${name}!
+            Responderei em breve no e-mail: ${email}.
+            Sua Mensagem: "${message}" foi recebida com sucesso.`;
+        alert(confirmationMessage);
+
+        form.reset();
+    });
+
+});
